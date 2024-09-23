a32ce83bbe93dcfa1ad782d33f2d4a8ff8395aab
TopTrenDev <nikkimoreno7866@gmail.com>
2024-09-24 04:53:01 +0900

    feat: add bot db playground

---
+++ src/utils/add-bot.js
@@ -0,0 +1,107 @@
+// MongoDB Playground
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
+
+// The current database to use.
+use("casino");
+
+// Create a new document in the collection.
+db.getCollection("userbots").insertOne({
+  email: "washingtonbilly@hotmail.com",
+  username: "Strawberry se",
+  verifiedPhoneNumber: "10-3515-0295",
+});
+db.getCollection("userbots").insertOne({
+  email: "kwokwingc@icloud.com",
+  username: "tango pro",
+  verifiedPhoneNumber: "157-2856-2865",
+});
+db.getCollection("userbots").insertOne({
+  email: "calv@outlook.com",
+  username: "Strawberry",
+  verifiedPhoneNumber: "	614-865-2898",
+});
+db.getCollection("userbots").insertOne({
+  email: "otss@gmail.com",
+  username: "Orange pro",
+  verifiedPhoneNumber: "	(151) 802 7469",
+});
+db.getCollection("userbots").insertOne({
+  email: "silva98@gmail.com",
+  username: "Rambutan",
+  verifiedPhoneNumber: "	20-376-1096",
+});
+db.getCollection("userbots").insertOne({
+  email: "porterra9@outlook.com",
+  username: "hambutan",
+  verifiedPhoneNumber: "	21-5130-8534",
+});
+db.getCollection("userbots").insertOne({
+  email: "chakf709@hotmail.com",
+  username: "Apple",
+  verifiedPhoneNumber: "	158-5320-4958",
+});
+db.getCollection("userbots").insertOne({
+  email: "trandia916@gmail.com",
+  username: "Orange",
+  verifiedPhoneNumber: "	(1865) 56 5107",
+});
+db.getCollection("userbots").insertOne({
+  email: "pangwingsze10@yahoo.com",
+  username: "iRtspberry",
+  verifiedPhoneNumber: "	7599 419576",
+});
+db.getCollection("userbots").insertOne({
+  email: "yuto60@hotmail.com",
+  username: "Ramfutan elite",
+  verifiedPhoneNumber: "	52-543-9417",
+});
+db.getCollection("userbots").insertOne({
+  email: "swsiu60@outlook.com",
+  username: "Mango premium",
+  verifiedPhoneNumber: "	614-075-4090",
+});
+db.getCollection("userbots").insertOne({
+  email: "itsukiya@gmail.com",
+  username: "Raypberry",
+  verifiedPhoneNumber: "	(20) 3522 7852",
+});
+db.getCollection("userbots").insertOne({
+  email: "szeyuhsuan@outlook.com",
+  username: "omni-Cherry",
+  verifiedPhoneNumber: "	3-5578-2272",
+});
+db.getCollection("userbots").insertOne({
+  email: "swalk@gmail.com",
+  username: "Pluqts",
+  verifiedPhoneNumber: "	80-6146-9085",
+});
+db.getCollection("userbots").insertOne({
+  email: "dchiba7@yahoo.com",
+  username: "Strawberry",
+  verifiedPhoneNumber: "	52-259-7872",
+});
+db.getCollection("userbots").insertOne({
+  email: "haradasakura@outlook.com",
+  username: "Mango plus",
+  verifiedPhoneNumber: "	90-7641-9125",
+});
+db.getCollection("userbots").insertOne({
+  email: "xiazhiyuan@gmail.com",
+  username: "lango mini",
+  verifiedPhoneNumber: "	213-636-5746",
+});
+db.getCollection("userbots").insertOne({
+  email: "waiyeet@hotmail.com",
+  username: "iOrange",
+  verifiedPhoneNumber: "	74-146-8633",
+});
+db.getCollection("userbots").insertOne({
+  email: "reynoldsemily02@gmail.com",
+  username: "Pluots se",
+  verifiedPhoneNumber: "	70-8526-2862",
+});
+db.getCollection("userbots").insertOne({
+  email: "hikki@gmail.com",
+  username: "fiwi pi",
+  verifiedPhoneNumber: "	21-773-6964",
+});
